8a60bad84c4c7a55d457e50c6743ac2ed407987a
Janzen <janzenchoi@yahoo.com>
2025-02-27 16:34:03 +1100

    Added resume option

--- scripts/main.py
+++ scripts/main.py
@@ -100,7 +100,9 @@ def main():
         # Add results from previous simulations
         sim_dict_list = [csv_to_dict(f"{dir_path}/summary.csv") for dir_path in dir_path_list]
         params_dict_list = [read_params(f"{dir_path}/params.txt") for dir_path in dir_path_list]
-        sim_dict_list = [{**params_dict, **sim_dict} for sim_dict, params_dict in zip(sim_dict_list, params_dict_list)]
+        for sim_dict, params_dict in zip(sim_dict_list, params_dict_list):
+            for key in PARAM_NAMES:
+                sim_dict[key] = params_dict[key]
 
         # Print
         num_init = len(init_dir_path_list)
